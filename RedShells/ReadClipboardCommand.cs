0c0cdee69bf4bef04a57bad62a9a34a1b21ed306
Marc Vitalis <marc.vitalis@live.com>
2011-02-17 13:56:26 +0800

    added readme and clipboard capability

---
+++ RedShells/ReadClipboardCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RedShells
+{
+    [Cmdlet(VerbsCommunications.Read, "Clipboard")]
+    public class ReadClipboardCommand : PSCmdlet
+    {
+
+        protected override void ProcessRecord()
+        {
+            TextBox clipStore = new TextBox();
+            clipStore.Multiline = true;
+            clipStore.Paste();
+
+            var files = clipStore.Text.Trim().Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var file in files)
+            {
+                WriteObject(string.Format("Copying {0}", file));
+                File.Copy(file, Path.Combine(SessionState.Path.CurrentLocation.Path, Path.GetFileName(file)), true);
+            }
+        }
+
+    }
+}

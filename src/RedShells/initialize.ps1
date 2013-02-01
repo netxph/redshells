023de86d8fc7315e7c0df27efc23816b5b63b24e
Marc Vitalis <marc.vitalis@live.com>
2013-02-01 15:34:57 +0800

    Setup project for development

---
+++ src/RedShells/initialize.ps1
@@ -0,0 +1,2 @@
+﻿import-module .\redshells.psd1
+.\redshells-scripts.ps1
---
+++ src/RedShells/redshells-scripts.ps1
@@ -0,0 +1,7 @@
+﻿[System.Console]::Title = ".NET Developer Console [RedShells 2.0]"
+
+cls
+
+".NET Developer Console [RedShells 2.0]"
+"Copyright (C) 2013 Marc Vitalis. All rights reserved."
+""

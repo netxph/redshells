968ec083f4ee501c4eeacba5797defb40e28e610
Marc Vitalis <marc.vitalis@live.com>
2013-02-01 14:35:25 +0800

    Initial project setup

--- rakefile.rb
+++ rakefile.rb
@@ -3,9 +3,7 @@ task :default => [:build]
 
 desc 'build'
 task :build do
-  Rake::Task["uninstall"].invoke
   sh 'msbuild'
-  Rake::Task["install"].invoke
 end
 
 desc 'rebuild'
@@ -13,12 +11,3 @@ task :rebuild do
   sh 'msbuild /t:clean;rebuild'
 end
 
-desc 'install'
-task :install do
-  sh 'installutil bin/debug/RedShells.dll'
-end
-
-desc 'uninstall'
-task :uninstall do
-  sh 'installutil bin/debug/RedShells.dll'
-end

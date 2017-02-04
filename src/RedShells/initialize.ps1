5759ab5e067d60e270be0eaa83bdf517146ecd98
Marc Abraham VITALIS <marc-abraham.vitalis@amadeus.com>
2017-02-04 21:01:10 +0800

    fresh start

--- src/RedShells/initialize.ps1
+++
@@ -1,2 +0,0 @@
-﻿import-module .\redshells.psd1
-.\redshells-scripts.ps1
--- src/RedShells/redshells-scripts.ps1
+++
@@ -1,7 +0,0 @@
-﻿[System.Console]::Title = ".NET Developer Console [RedShells 2.0]"
-
-cls
-
-".NET Developer Console [RedShells 2.0]"
-"Copyright (C) 2013 Marc Vitalis. All rights reserved."
-""

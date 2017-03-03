24714314f1bd5ee98a7a1b2173925947cc41bf77
Marc Vitalis <marc.vitalis@live.com>
2017-03-03 13:59:31 +0800

    add sqlitebootstrap

---
+++ src/RedShells.Data/SqliteBootstrap.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Data;
+
+namespace RedShells.Data
+{
+
+    public class SqliteBootstrap
+    {
+
+        public SqliteBootstrap(IDbConnection connection)
+        {
+        }
+    }
+}
--- src/RedShells/WorkspaceRepository.cs
+++ src/RedShells/WorkspaceRepository.cs
@@ -9,8 +9,24 @@ namespace RedShells
     public class WorkspaceRepository : IWorkspaceRepository
     {
 
+        private readonly IDbConnection _connection;
+
+        protected IDbConnection Connection { get { return _connection; } }
+
         public WorkspaceRepository(IDbConnection connection)
         {
+            if(connection == null)
+            {
+                throw new ArgumentNullException("connection", "WorkspaceRepository:connection");
+            }
+
+            OnInitialize(connection);
+            _connection = connection;
+        }
+
+        protected virtual void OnInitialize(IDbConnection connection)
+        {
+            var bootstrap = new Data.SqliteBootstrap(connection);
         }
 
         public Workspace Get(string name)

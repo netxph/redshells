b482ff22621912a945035394ae18fb50f8c61393
Marc Vitalis <marc.vitalis@live.com>
2017-03-08 13:43:31 +0800

    add test console

---
+++ src/RedShells.Host/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace RedShells.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var ps = PowerShell.Create();
+            ps.AddCommand("Get-ChildItem");
+            ps.Invoke();
+        }
+    }
+}

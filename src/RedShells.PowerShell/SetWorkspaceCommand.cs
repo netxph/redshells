39ef43fc4688296898872ba5513d2e64c05aa089
Marc Vitalis <marc.vitalis@live.com>
2017-03-10 11:09:27 +0800

    imlement set-workspace

--- src/RedShells.PowerShell/SetWorkspaceCommand.cs
+++ src/RedShells.PowerShell/SetWorkspaceCommand.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Management.Automation;
+using RedShells.Data;
+using RedShells.Core.Interfaces;
 
 namespace RedShells.PowerShell
 {
@@ -8,10 +10,32 @@ namespace RedShells.PowerShell
     [Cmdlet(VerbsCommon.Set, "Workspace")]
     public class SetWorkspaceCommand: PSCmdlet
     {
+        private readonly IWorkspaceRepository _repository;
+
+        protected IWorkspaceRepository Repository { get { return _repository; } }
+
+        public SetWorkspaceCommand()
+            : this(new JsonWorkspaceRepository("workspace.json"))
+        {
+        }
+
+        public SetWorkspaceCommand(IWorkspaceRepository repository)
+        {
+            _repository = repository;
+        }
+
+        
+        [Parameter(Position = 0, Mandatory = true)]
+        public string Name { get; set; }
 
         protected override void ProcessRecord()
         {
-            WriteObject("Hello world!!!");
+            var workspace = Repository.Get(Name);
+
+            if(workspace != null)
+            {
+                this.InvokeCommand.InvokeScript($"Set-Location -Path '{workspace.Directory}'");
+            }
         }
     }
 }
